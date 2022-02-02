509a0a08a4100579ab317f181406204003136487
Jose Estrada <j.estrada.developer@gmail.com>
2022-02-02 17:11:28 -0600

    Add Standard Linter Adding stantard linter and vercel.json for deployment on vercel

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render( <h1>Impresion de etiqueta con los presets de react! </h1>, document.getElementById('app'))
+ReactDOM.render(
+  <h1>Impresion de etiqueta con los presets de react y deploy a Vercel!</h1>,
+  document.getElementById('app')
+)
