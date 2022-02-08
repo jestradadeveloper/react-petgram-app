6643ff9601d75214e0b488c205980ad328d36a36
Jose Estrada <j.estrada.developer@gmail.com>
2022-02-08 16:29:12 -0600

    Add Use Effect for fixing elements on scroll Adding use effect for fixing categories component on scroll

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -3,19 +3,33 @@ import { Category } from '../Category'
 import { List, Item } from './styles'
 export const ListOfCategories = () => {
   const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
   useEffect(function () {
-    window.fetch('https://petagram-jestradadeveloper.vercel.app/categories')
+    window.fetch('https://petgram-server-jestradadeveloper.vercel.app/categories')
       .then(res => res.json())
       .then(response => {
         setCategories(response)
       })
   }, [])
-
-  return (
-    <List>
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+    document.addEventListener('scroll', onScroll)
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+  const renderList = (fixed) => (
+    <List fixed={fixed}>
       {
         categories.map(category => <Item key={category.id}> <Category {...category} /></Item>)
       }
     </List>
+  )
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -1,4 +1,5 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { bounceDown } from '../../styles/animation'
 export const List = styled.ul`
   display: flex;
   overflow: scroll;
@@ -6,6 +7,24 @@ export const List = styled.ul`
   &::-webkit-scrollbar {
     display: none;
   }
+  ${props => props.fixed && css`
+    {
+      ${bounceDown('.3s', 'ease')}
+      background: #fff;
+      padding: 10px;
+      border-radius: 40px;
+      box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+      left: 0;
+      margin: 0 auto;
+      max-width: 450px;
+      position:fixed;
+      right:0;
+      top: 0px;
+      transform: scale(.5);
+      z-index: 1;
+    }
+  `}
+
 `
 export const Item = styled.li`
   padding: 0 10px;
