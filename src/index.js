887b8bffb5be4974ad9c8ad21f85a5927b8619b1
Jose Estrada <j.estrada.developer@gmail.com>
2022-02-10 17:57:56 -0600

    Add Apollo Client and Graphql Add apollo client and graphql and use the apollo client to fetch data on ListOfPhotoCards component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 import { App } from './App'
+
+const client = new ApolloClient({
+  uri: 'https://petgram-server-jestradadeveloper.vercel.app/graphql',
+  cache: new InMemoryCache()
+})
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+  ,
   document.getElementById('app')
 )
